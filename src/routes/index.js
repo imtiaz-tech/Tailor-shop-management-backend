a3b2ec9e024474a17291f7c6dca015997880131c
Imtiaz Ahmad <imtiazahmadswl93@gmail.com>
2024-10-17 19:58:49 +0500

    change files names in project

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,16 @@
 import express from 'express';
 import auth from './auth';
 import user from './users';
-import products from './products';
+import measurement_orders from './measurement_orders';
 import unauthrized from './unauthrized';
-import order from './order'
+import customer_users from './customer_users'
 import { authenticateAuthToken } from '../middlewares/auth';
 
 
 const router = express.Router();
 router.use('/auth', auth);
 router.use('/users', authenticateAuthToken, user);
-router.use('/products', authenticateAuthToken, products);
+router.use('/measurement_orders', authenticateAuthToken, measurement_orders);
 router.use('/unauthrized', unauthrized);
-router.use('/orders',authenticateAuthToken,order)
+router.use('/customer_users',authenticateAuthToken,customer_users)
 export default router;

10ea610e31210e8eb93585691a772c6ba634cac1
Imtiaz Ahmad <imtiazahmadswl93@gmail.com>
2024-11-20 02:28:10 +0500

    changes

--- src/config/aws-s3.js
+++ src/config/aws-s3.js
@@ -1,11 +1,11 @@
 import { S3Client } from "@aws-sdk/client-s3";
-const { AWS_ACCESS_KEY, AWS_SECRET_KEY } = process.env;
+const { AWS_ACCESS_TOKEN, AWS_SECRET_TOKEN } = process.env;
 
 const credentials = {
   region: "us-east-1",
   credentials: {
-    accessKeyId: AWS_ACCESS_KEY,
-    secretAccessKey: AWS_SECRET_KEY,
+    accessKeyId: AWS_ACCESS_TOKEN,
+    secretAccessKey: AWS_SECRET_TOKEN,
   },
 };
 
